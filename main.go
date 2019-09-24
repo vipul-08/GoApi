6983693b8663ca5fc0665de742db284ab82f29fa
vipul-08 <singh.vipul15@siesgst.ac.in>
2019-09-24 21:32:16 +0530

    back

--- main.go
+++ main.go
@@ -8,18 +8,13 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
-
-//RandomResponse is a model for any random response.
-type RandomResponse struct {
-	resp   string
-	status int
-}
 
 //User is a model for users.
 type User struct {
@@ -34,7 +29,7 @@ type Joke struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Joketitle string             `json:"jokeTitle,omitempty" bson:"jokeTitle,omitempty"`
 	Jokevalue string             `json:"jokeString,omitempty" bson:"jokeString,omitempty"`
-	Userid    string             `json:"userId,omitempty" bson:"userId,omitempty"`
+	Username  string             `json:"userName,omitempty" bson:"userName,omitempty"`
 }
 
 //CreateUserEndpoint is used for registration.
@@ -47,6 +42,74 @@ func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	defer cancel()
 	result, _ := collection.InsertOne(ctx, user)
 	json.NewEncoder(response).Encode(result)
+}
+
+//CreateJokeEndpoint is used for registration.
+func CreateJokeEndpoint(response http.ResponseWriter, request *http.Request) {
+
+	response.Header().Set("Access-Control-Allow-Origin", request.Header.Get("Origin"))
+	response.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+	response.Header().Set("content-type", "application/json")
+	var joke Joke
+	_ = json.NewDecoder(request.Body).Decode(&joke)
+	collection := client.Database("projectDb").Collection("jokes")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	result, _ := collection.InsertOne(ctx, joke)
+	json.NewEncoder(response).Encode(result)
+}
+
+//DeleteJokeEndpoint is used for deleting joke.
+func DeleteJokeEndpoint(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("delete")
+	response.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	response.Header().Set("Access-Control-Allow-Methods", "OPTIONS,DELETE")
+	response.Header().Set("content-type", "application/json")
+	var joke Joke
+	_ = json.NewDecoder(request.Body).Decode(&joke)
+	collection := client.Database("projectDb").Collection("jokes")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	result, _ := collection.DeleteOne(ctx, Joke{ID: joke.ID})
+	json.NewEncoder(response).Encode(result)
+}
+
+// GetAllJokesEndpoint returns all the jokes...
+func GetAllJokesEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Access-Control-Allow-Origin", request.Header.Get("Origin"))
+	response.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+	response.Header().Set("content-type", "application/json")
+	var jokes []Joke
+	collection := client.Database("projectDb").Collection("jokes")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var joke Joke
+		cursor.Decode(&joke)
+		jokes = append(jokes, joke)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(jokes)
+}
+
+//OptionsEndpoint is method .
+func OptionsEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Access-Control-Allow-Origin", request.Header.Get("Origin"))
+	response.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	fmt.Println("options")
 }
 
 //LoginUserEndpoint is used for login.
@@ -81,5 +144,9 @@ func main() {
 
 	router.HandleFunc("/user", CreateUserEndpoint).Methods("POST")
 	router.HandleFunc("/login", LoginUserEndpoint).Methods("POST")
+	router.HandleFunc("/joke", CreateJokeEndpoint).Methods("POST")
+	router.HandleFunc("/joke", GetAllJokesEndpoint).Methods("GET")
+	router.HandleFunc("/joke", DeleteJokeEndpoint).Methods("DELETE")
+	router.HandleFunc("/joke", OptionsEndpoint).Methods("OPTIONS")
 	http.ListenAndServe(":5000", router)
 }
